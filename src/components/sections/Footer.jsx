fdbcac014d93bcf90b5d55d3756e07c8c76a3ad0
notHeisenberg <mahfuzantor2@gmail.com>
2026-03-10 00:57:29 +0600

    Update Footer component to link to Privacy Policy instead of Service Contract

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -85,7 +85,7 @@ const Footer = () => {
               to="privacy-policy"
               className="underline hover:text-gray-300 transition-colors duration-200"
             >
-              {t('footer.contact.serviceContract')}
+              {t('footer.contact.privacyPolicy')}
             </Link>
           </p>
         </div>
